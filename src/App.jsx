eb5d9711ac87d74b66baddc50f0d83aad4ce9b6a
Ge01ge <georgea080@gmail.com>
2022-11-04 10:23:26 -0300

    1er comit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+import navBar from './components/navBar';
+
+function App() {
+  return (
+    <navBar />
+  );
+}
+
+export default App;
---
+++ src/components/navBar/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const navBar = () =>{
+    return (
+        <h1>navBar</h1>
+    );
+}
+export default navBar;
+
