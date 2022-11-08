0c5cceb29d12f99c63e364b1ca6206c5d0ae28fd
Ge01ge <georgea080@gmail.com>
2022-11-08 11:25:17 -0300

    module.css funcionando

--- src/components/CartWidget/index.jsx
+++ src/components/CartWidget/index.jsx
@@ -4,7 +4,7 @@ import styles from "./CartIcon.module.css";
 
 export const CartWidget = () =>{
     return (
-        <i className="bi bi-cart-plus-fill pt-2 {styles.cart_icon}" ></i>
+        <i className={`bi bi-cart-plus-fill pt-2 ${styles.cart_icon}`} ></i>
     );
     // style={{width: 100 !important, height: 100  !important,
     //     paddingTop: 50  !important,}}
