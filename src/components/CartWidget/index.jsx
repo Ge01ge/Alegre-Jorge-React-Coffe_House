95b72d3531d7f3f620420a0b7e56bedcbc14ddc8
Ge01ge <georgea080@gmail.com>
2022-11-08 11:07:42 -0300

    probando module.css

--- src/components/CartWidget/index.jsx
+++ src/components/CartWidget/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-import  CartIcon from './CartIcon.css';
+import styles from "./CartIcon.module.css";
 
 export const CartWidget = () =>{
     return (
-        <i className="bi bi-cart-plus-fill cart_icon"></i>
+        <i className="bi bi-cart-plus-fill pt-2 {styles.cart_icon}" ></i>
     );
+    // style={{width: 100 !important, height: 100  !important,
+    //     paddingTop: 50  !important,}}
 
 }
 export default CartWidget;
