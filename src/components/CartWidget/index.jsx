89f6fc60c692a16b14d30a3ff255e30e9576a4b2
Ge01ge <georgea080@gmail.com>
2022-11-08 11:35:50 -0300

    sass funcionando

--- src/components/CartWidget/index.jsx
+++ src/components/CartWidget/index.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 
-import styles from "./CartIcon.module.css";
+import styles from "./CartIcon.module.scss";
 
 export const CartWidget = () =>{
     return (
         <i className={`bi bi-cart-plus-fill pt-2 ${styles.cart_icon}`} ></i>
     );
-    // style={{width: 100 !important, height: 100  !important,
-    //     paddingTop: 50  !important,}}
-
+    
 }
 export default CartWidget;
