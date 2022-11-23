de377f59f610165c9f215ce73b1eb6be8e1ad0e5
Ge01ge <georgea080@gmail.com>
2022-11-23 19:04:54 -0300

    estilo 1.2

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,17 +2,20 @@ import styles from "../ItemDetail/ItemDetail.module.scss";
 
 const Item = ({ product }) => {
   return (
-    <div className="d-inline-flex">
-      <div className={`mx-auto ${styles.cart1}`}>
-        <div className="card col-md-8 mx-auto border border-dark bg-warning text-dark bg-opacity-10">
+
+    // className={`d-inline-flex ${styles.espacio}`}
+   
+    <div className={`d-inline-flex ${styles.espacio}`}>
+      <div className={`col-md-4 ${styles.cart1}`}>
+        <div className="card col-md-8 border border-dark bg-warning text-dark bg-opacity-10">
           <h5 className="card-title px-2">{product.name}</h5>
           <img
             src={product.img}
             alt={product.description}
-            className={`mx-auto ${styles.img}`}
+            className={`mx-auto ${styles.img1}`}
           />
           <p className="card-text px-2">En Stock: {product.stock} Unidades</p>
-          <p className="card-text px-2">Precio: {product.price}</p>
+          <p className="card-text px-2">Precio: ${product.price}</p>
           <button type="button" className="btn btn-dark px-4 mx-4 my-2">
             Ver Detalle
           </button>
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,13 +1,16 @@
 import Item from "../Item/Item";
+// import styles from "../ItemDetail/ItemDetail.module.scss";
 
 const ItemList = ({ products }) => {
   return (
-  <ul className="flex items-center justify-center gap-4">
-  {products.map((product) => (
-  <Item key={product.id} product={product} />
-  ))}
-  
-  </ul>
+
+    // className={` ${styles.listContainer}`}
+    <ul className="flex justify-content-start align-items-center flex-wrap" >
+    {products.map((product) => (
+    <Item key={product.id} product={product} />
+    ))}
+    
+    </ul>
   );
   }; 
 
